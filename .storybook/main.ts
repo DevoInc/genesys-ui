614bc11ce1daed75b32416ae4a5c784e01708bd9
Javier López Úbeda <jlopezcur@gmail.com>
2023-03-03 17:18:02 +0100

    build: added pre-cached deps to sb vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: [
@@ -19,16 +20,33 @@ const config: StorybookConfig = {
     '@storybook/addon-a11y',
     '@storybook/preset-scss',
   ],
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: [
+          '@storybook/theming',
+          '@devoinc/genesys-brand-devo',
+          '@storybook/addon-essentials/docs/mdx-react-shim',
+          '@storybook/blocks',
+          '@devoinc/genesys-ui',
+          '@devoinc/genesys-ui-datetime',
+          '@devoinc/genesys-ui-form',
+        ],
+      },
+    });
+  },
   typescript: {
-  //   /**
-  //    * Solves TS extended declarations coming from node_modules not showing in SB
-  //    * https://github.com/storybookjs/storybook/issues/12129
-  //    * */
-  //   reactDocgen: 'react-docgen-typescript',
-  //   reactDocgenTypescriptOptions: {
-  //     // this is required to show values from type declarations
-  //     shouldExtractLiteralValuesFromEnum: true,
-  //   },
+    //   /**
+    //    * Solves TS extended declarations coming from node_modules not showing in SB
+    //    * https://github.com/storybookjs/storybook/issues/12129
+    //    * */
+    //   reactDocgen: 'react-docgen-typescript',
+    //   reactDocgenTypescriptOptions: {
+    //     // this is required to show values from type declarations
+    //     shouldExtractLiteralValuesFromEnum: true,
+    //   },
     check: true,
   },
   staticDirs: ['assets'],
