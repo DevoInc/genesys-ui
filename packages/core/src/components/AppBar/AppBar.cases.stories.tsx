3a04caa0674a2e4beca57a38339d17b289fc3b99
Gonzalo Uceda <gonzalo.uceda@devo.com>
2023-03-06 10:31:50 +0100

    docs(core/AppBar): improve storybook docs

--- packages/core/src/components/AppBar/AppBar.cases.stories.tsx
+++ packages/core/src/components/AppBar/AppBar.cases.stories.tsx
@@ -1,19 +1,20 @@
 import { Meta, StoryObj } from '@storybook/react';
 
 import { AppBar } from './AppBar';
-import { mainActions, tabs } from './__stories__/content';
+import { mainActions, customBlock, userOptions } from './__stories__/content';
 
 const meta: Meta<typeof AppBar> = {
-  title: 'Components/Core/Navigation/AppBar',
+  title: 'Components/Core/Navigation/AppBar/Cases',
   component: AppBar,
 };
 
 export default meta;
 type Story = StoryObj<typeof AppBar>;
 
-export const Base: Story = {
+export const CustomContent: Story = {
   args: {
+    customContent: customBlock,
     mainActions,
-    tabs,
+    userOptions,
   },
 };
