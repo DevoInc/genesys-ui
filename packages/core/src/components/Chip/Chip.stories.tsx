47f11db34c8a5561bbf247167459f7c2ea2b20c0
Horacio Gouveia <horacio.gouveia@devo.com>
2023-03-06 10:54:49 +0100

    docs(Chip): improve component doc

--- packages/core/src/components/Chip/Chip.stories.tsx
+++ packages/core/src/components/Chip/Chip.stories.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 
 import { Chip } from '..';
@@ -19,56 +18,3 @@ export default meta;
 type Story = StoryObj<typeof Chip>;
 
 export const Base: Story = {};
-
-export const MultipleControlled: Story = {
-  render: () =>
-    (() => {
-      const [selected, setSelected] = React.useState(false);
-      return (
-        <Chip
-          icon="like_heart_favorite_rating_love"
-          iconSelected="heart_full"
-          onChange={() => setSelected(!selected)}
-          state={selected ? 'selected' : 'enabled'}
-          value="favorite"
-        >
-          Favorite
-        </Chip>
-      );
-    })(),
-};
-
-export const MultipleUncontrolled: Story = {
-  args: {
-    icon: 'like_heart_favorite_rating_love',
-    children: 'Favorite',
-  },
-};
-
-export const SingleControlled: Story = {
-  render: () =>
-    (() => {
-      const [selected, setSelected] = React.useState(false);
-      return (
-        <Chip
-          icon="like_heart_favorite_rating_love"
-          iconSelected="heart_full"
-          name="single"
-          onChange={() => setSelected(!selected)}
-          selectionScheme="single"
-          state={selected ? 'selected' : 'enabled'}
-        >
-          Favorite
-        </Chip>
-      );
-    })(),
-};
-
-export const SingleUncontrolled: Story = {
-  args: {
-    icon: 'like_heart_favorite_rating_love',
-    children: 'Favorite',
-    selectionScheme: 'single',
-    name: 'single',
-  },
-};
