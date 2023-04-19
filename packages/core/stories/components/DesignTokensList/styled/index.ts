e98c5e6267e32f00c2d8ee0e0d03edcc418b641b
Javier López Úbeda <jlopezcur@gmail.com>
2023-04-19 10:32:56 +0200

    chore: removed unused code

--- packages/core/stories/components/DesignTokensList/styled/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './StyledDesignTokensContainer';
-export * from './StyledDesignTokensTable';
-export * from './StyledDesignTokensCell';
-export * from './StyledDesignTokensHeadCell';
-export * from './StyledDesignTokensRow';
-export * from './StyledDesignTokensThead';
