8e19283f1ec7b15e9ec3c0b3c95bb399a773d139
Jose Andrés Granero <joseandres.granero@devo.com>
2023-05-18 11:45:29 +0200

    fix(TextareControl): fixed not necessary css

--- packages/core/src/components/TextareaControl/TextareaControl.stories.tsx
+++ packages/core/src/components/TextareaControl/TextareaControl.stories.tsx
@@ -26,7 +26,7 @@ export const Base: Story = {
   render: (args) =>
     ((args) => (
       <VFlex maxWidth="72rem">
-        <TextareaControl {...args} css={'border-color: red'} />
+        <TextareaControl {...args} />
       </VFlex>
     ))(args),
   args: {
