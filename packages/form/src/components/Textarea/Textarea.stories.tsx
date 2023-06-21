1c8f0788b41b163f0ad5f9147c24354d4bb1c0ee
Carlos Pinedo <carlos.pinedo@devo.com>
2023-06-21 15:44:11 +0200

    feat(Textarea): QUV-1099 Added example for character counter

--- packages/form/src/components/Textarea/Textarea.stories.tsx
+++ packages/form/src/components/Textarea/Textarea.stories.tsx
@@ -1,6 +1,7 @@
+import * as React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
-
 import { Textarea } from './Textarea';
+import { FlexItem, HFlex, Typography } from '@devoinc/genesys-ui';
 
 const meta: Meta<typeof Textarea> = {
   title: 'Components/Form/Textarea',
@@ -34,3 +35,45 @@ export default meta;
 type Story = StoryObj<typeof Textarea>;
 
 export const Base: Story = {};
+export const UsingACharacterCounter: Story = {
+  render: (args) =>
+    ((args) => {
+      const MAX_CHARACTERS = 10;
+      const [counter, setCounter] = React.useState<number>(MAX_CHARACTERS);
+      const [value, setValue] = React.useState('');
+
+      const getFixedValue = (newValue: string) =>
+        newValue.length > MAX_CHARACTERS ? value : newValue;
+      const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(getFixedValue(event.target.value));
+        setCounter(MAX_CHARACTERS - event.target.value.length);
+      };
+
+      return (
+        <Textarea
+          {...args}
+          onChange={onChange}
+          helper={
+            <HFlex justifyContent={'space-between'} width={'100%'}>
+              <FlexItem alignSelf={'flex-start'}>
+                <Typography.Paragraph
+                  colorScheme={counter < 0 ? 'error' : 'weak'}
+                >
+                  {args.helper || `Max ${MAX_CHARACTERS} characters`}
+                </Typography.Paragraph>
+              </FlexItem>
+              <FlexItem alignSelf={'flex-end'}>
+                <Typography.Paragraph
+                  colorScheme={counter < 0 ? 'error' : 'weak'}
+                >
+                  {counter < 0 ? 0 : counter}
+                </Typography.Paragraph>
+              </FlexItem>
+            </HFlex>
+          }
+          status={counter < 0 ? 'error' : 'base'}
+          value={value}
+        />
+      );
+    })(args),
+};
