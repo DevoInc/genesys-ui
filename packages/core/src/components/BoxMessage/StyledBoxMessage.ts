ddaa712d4c2164e9cb242b744fa12a1d37b4a8bb
Jose Andrés Granero <joseandres.granero@devo.com>
2023-07-11 10:27:47 +0200

    fix: uncommented code because now there are available the component tokens

--- packages/core/src/components/BoxMessage/StyledBoxMessage.ts
+++ packages/core/src/components/BoxMessage/StyledBoxMessage.ts
@@ -10,20 +10,14 @@ export interface StyledBoxMessageProps {
 export const StyledBoxMessage = styled.div<StyledBoxMessageProps>`
   ${({ status = 'info', theme }) => {
     const cmpTokens = theme.cmp.boxMessage;
-
-    // return css`
-    //   position: relative;
-    //   display: flex;
-    //   gap: ${cmpTokens.space.padding};
-    //   border: ${cmpTokens.shape.borderSize} solid
-    //     ${cmpTokens.color.border[status]};
-    //   border-radius: ${cmpTokens.shape.borderRadius};
-    //   padding: ${cmpTokens.space.padding};
-    //   background-color: ${cmpTokens.color.background[status]};
-    // `;
     return css`
       position: relative;
       display: flex;
+      gap: ${cmpTokens.space.padding};
+      border: ${cmpTokens.shape.borderSize} solid
+        ${cmpTokens.color.border[status]};
+      border-radius: ${cmpTokens.shape.borderRadius};
+      padding: ${cmpTokens.space.padding};
       background-color: ${cmpTokens.color.background[status]};
     `;
   }}
