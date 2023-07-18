f62adefcea0aedc9ed6e3f87e99b0fb001fb1a70
Carlos Pinedo <carlos.pinedo@devo.com>
2023-07-19 00:41:58 +0200

    test(Helper): Added cases for conditional message rendering

---
+++ packages/core/src/components/Helper/Helper.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Helper } from './Helper';
+import { render, screen } from '@testing-library/react';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+import { dark } from '@devoinc/genesys-brand-devo';
+
+const MESSAGE_STRING = 'This is a test message';
+const MESSAGE_REACT = <span>{MESSAGE_STRING}</span>;
+const TEST_ID = 'helper';
+
+const theme: DefaultTheme = dark;
+
+describe('Helper', () => {
+  test('Passing a string as message', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Helper data-testid={TEST_ID} message={MESSAGE_STRING} />
+      </ThemeProvider>
+    );
+    expect(screen.getAllByTestId(TEST_ID)[0].children[0].tagName).toBe('P');
+    expect(screen.getAllByTestId(TEST_ID)[0].children[0].innerHTML).toBe(
+      MESSAGE_STRING
+    );
+  });
+
+  test('Passing a React fragment as message', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Helper data-testid={TEST_ID} message={MESSAGE_REACT} />
+      </ThemeProvider>
+    );
+    expect(screen.getAllByTestId(TEST_ID)[0].innerHTML).toBe(
+      `<span>${MESSAGE_STRING}</span>`
+    );
+  });
+});
