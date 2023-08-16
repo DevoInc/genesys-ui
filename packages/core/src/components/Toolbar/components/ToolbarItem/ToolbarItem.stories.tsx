0839a3a99c06ac4c629e179643dae7ded766c6a8
Gonzalo Uceda <gonzalo.uceda@devo.com>
2023-08-16 14:05:00 +0200

    build: fix story

---
+++ packages/core/src/components/Toolbar/components/ToolbarItem/ToolbarItem.stories.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Toolbar } from '../../..';
+
+const meta: Meta<typeof Toolbar.Item> = {
+  title: 'Components/Core/Layout/Toolbar/Subcomponents',
+  component: Toolbar.Item,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Toolbar.Item>;
+
+export const Item: Story = {
+  render: (args) => (
+    <Toolbar>
+      <Toolbar.Item {...args}>Root</Toolbar.Item>
+      <Toolbar.Group>
+        <Toolbar.Item {...args}>Grouped 1</Toolbar.Item>
+        <Toolbar.Item {...args}>Grouped 2</Toolbar.Item>
+      </Toolbar.Group>
+    </Toolbar>
+  ),
+};
