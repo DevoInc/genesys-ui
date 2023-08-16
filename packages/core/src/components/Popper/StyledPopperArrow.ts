a41066e8f40d897236d1d8ce764a2a3c3bb69fc4
pablillosaez <139959967+pablillosaez@users.noreply.github.com>
2023-08-16 14:43:18 +0200

    fix(Popper): QUV-1095 incorrect position on mount (#23)
    * fix(Popper): QUV-1095 incorret position on mount
    
    * fix(Popper): QUV-1095 incorret position on mount

--- packages/core/src/components/Popper/StyledPopperArrow.ts
+++ packages/core/src/components/Popper/StyledPopperArrow.ts
@@ -9,31 +9,40 @@ export interface StyledPopperArrowProps {
 }
 
 export const StyledPopperArrow = styled.div<StyledPopperArrowProps>`
-  ${({ placement, theme, zIndex }) => css`
-    --arrow-position: calc(100% - 0.1rem);
+  ${({ placement, theme, zIndex }) => {
+    const tokens = theme.cmp.inlineMessage;
+    const arrowSize = tokens.arrow.size.square;
+    return css`
+      height: calc(${arrowSize} * 2);
+      width: calc(${arrowSize} * 2);
 
-    z-index: calc(
-      ${zIndex ? 0 : theme.alias.elevation.zIndex.depth.activated} + 1
-    );
+      z-index: calc(
+        ${zIndex ? 0 : theme.alias.elevation.zIndex.depth.activated} + 1
+      );
 
-    ${placement?.includes('top') &&
-    css`
-      top: var(--arrow-position);
-    `};
+      ${placement?.includes('top') &&
+      `
+        --arrow-position: calc(100% - 0.1rem);
+        top: var(--arrow-position);
+      `};
 
-    ${placement?.includes('bottom') &&
-    css`
-      bottom: var(--arrow-position);
-    `};
+      ${placement?.includes('bottom') &&
+      `
+        --arrow-position: calc(100% - 0.9rem);
+        bottom: var(--arrow-position);
+      `};
 
-    ${placement?.includes('right') &&
-    css`
-      right: var(--arrow-position);
-    `};
+      ${placement?.includes('right') &&
+      `
+        --arrow-position: calc(100% - 0.9rem);
+        right: var(--arrow-position);
+      `};
 
-    ${placement?.includes('left') &&
-    css`
-      left: var(--arrow-position);
-    `};
-  `};
+      ${placement?.includes('left') &&
+      `
+        --arrow-position: calc(100% - 0.1rem);
+        left: var(--arrow-position);
+      `};
+    `;
+  }};
 `;
