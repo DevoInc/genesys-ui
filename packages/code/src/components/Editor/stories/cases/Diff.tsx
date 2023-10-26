f6e271232a512c7881fa2ea84fd7a80a55f619f0
Gonzalo Uceda <gonzalo.uceda@devo.com>
2023-10-26 11:38:05 +0200

    build: fix linter

--- packages/code/src/components/Editor/stories/cases/Diff.tsx
+++ packages/code/src/components/Editor/stories/cases/Diff.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
-import { DiffEditor, type DiffEditorProps } from '../../../DiffEditor/DiffEditor';
+import {
+  DiffEditor,
+  type DiffEditorProps,
+} from '../../../DiffEditor/DiffEditor';
 
 export const Diff = ({ ...props }: DiffEditorProps) => {
   return <DiffEditor {...props} />;
