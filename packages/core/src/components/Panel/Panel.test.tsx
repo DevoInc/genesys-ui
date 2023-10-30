e890b1893971958d92714e1eac03fb50cb9dd0d2
Carlos Pinedo <carlos.pinedo@devo.com>
2023-10-30 18:27:58 +0100

    test(Panel): Added cases checking inner parts

--- packages/core/src/components/Panel/Panel.test.tsx
+++ packages/core/src/components/Panel/Panel.test.tsx
@@ -9,4 +9,22 @@ describe('Panel', () => {
     const { container } = render(<Panel>{'This is a panel'}</Panel>);
     expect(container.getElementsByTagName('div')[0]).toBeInTheDocument();
   });
+
+  test('Header renders inside PanelContainer', () => {
+    const { container } = render(<Panel>{'This is a panel'}</Panel>);
+    const panelContainer = container.getElementsByTagName('div')[0];
+    expect(panelContainer.getElementsByTagName('header')).toHaveLength(1);
+  });
+
+  test('Body renders inside PanelContainer', () => {
+    const { container } = render(<Panel>{'This is a panel'}</Panel>);
+    const panelContainer = container.getElementsByTagName('div')[0];
+    expect(panelContainer.getElementsByTagName('div')).toHaveLength(1);
+  });
+
+  test('Footer renders inside PanelContainer', () => {
+    const { container } = render(<Panel>{'This is a panel'}</Panel>);
+    const panelContainer = container.getElementsByTagName('div')[0];
+    expect(panelContainer.getElementsByTagName('footer')).toHaveLength(1);
+  });
 });
