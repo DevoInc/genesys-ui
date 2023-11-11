9b43b4987c7b22d6bdce4ccce0925a90f5aa3906
Carlos Pinedo <carlos.pinedo@devo.com>
2023-11-11 01:37:27 +0100

    docs(Table): Removed extra columns from story

--- packages/table/src/recipes/Table.stories.tsx
+++ packages/table/src/recipes/Table.stories.tsx
@@ -29,26 +29,6 @@ const data = Holo.of()
         ],
         Holo.chance.integer({ min: 1, max: 4 }),
       ),
-    id2: () => index++,
-    menu2: 'bool',
-    booleanValue2: 'bool',
-    name2: 'name',
-    age2: 'age',
-    company2: 'company',
-    balance2: 'euro',
-    status2: () => Holo.chance.pickone(['TODO', 'inProgress', 'test', 'done']),
-    picture2: 'avatar',
-    timestamp2: 'timestamp',
-    tags2: () =>
-      Holo.chance.pickset(
-        [
-          { text: 'Coworker', colorScheme: 'success' },
-          { text: 'Developer', colorScheme: 'warning' },
-          { text: 'Engineer', colorScheme: 'error' },
-          { text: 'Components', colorScheme: 'data-blue' },
-        ],
-        Holo.chance.integer({ min: 1, max: 4 }),
-      ),
   })
   .repeat(200)
   .generate();
@@ -143,83 +123,6 @@ const tableOptions: TableOptionsProps = {
     },
     {
       id: 'tags',
-      headerName: 'Tags',
-      type: 'tags',
-    },
-    {
-      id: 'id2',
-      type: 'text',
-      headerName: 'ID',
-    },
-    {
-      id: 'menu2',
-      headerName: 'Menu',
-      CellRenderer: (params) => {
-        return (
-          <DropdownMenu
-            items={[
-              {
-                type: 'itemSelectable',
-                label: 'Testing menu',
-                shortcut: "⌘ '",
-                title: 'Testing menu',
-                state: 'featured',
-                onChange: () => {
-                  console.log(params.columnDef.headerName);
-                },
-                defaultChecked: true,
-              },
-            ]}
-            label="Actions"
-            triggerComponent={Button}
-          />
-        );
-      },
-    },
-    {
-      id: 'booleanValue2',
-      headerName: 'Boolean value',
-      type: 'tagBoolean',
-    },
-    {
-      id: 'name2',
-      headerName: 'Name',
-      type: 'text',
-      editable: true,
-    },
-    {
-      id: 'age2',
-      headerName: 'Age',
-      type: 'number',
-    },
-    {
-      id: 'company2',
-      headerName: 'Company',
-      type: 'text',
-    },
-    {
-      id: 'balance2',
-      headerName: 'Balance',
-      type: 'number',
-    },
-    {
-      id: 'status2',
-      headerName: 'Status',
-      type: 'tag',
-    },
-    {
-      id: 'picture2',
-      headerName: 'Image (URL)',
-      type: 'link',
-    },
-    {
-      id: 'timestamp2',
-      headerName: 'Date',
-      type: 'date',
-      editable: true,
-    },
-    {
-      id: 'tags2',
       headerName: 'Tags',
       type: 'tags',
     },
