ee8133582b0adc94769537ab3e16e2f25b2327a4
santiago.trigo <santiago.trigo@devo.com>
2023-12-11 17:28:51 +0100

    test: refactor styled

--- packages/table/src/core/Table/StyledTable.ts
+++ packages/table/src/core/Table/StyledTable.ts
@@ -3,12 +3,12 @@ import * as React from 'react';
 
 export interface StyledTableProps {
   $width?: React.CSSProperties['width'];
-  minWidth?: number;
+  minWidth?: React.CSSProperties['minWidth'];
   $height?: React.CSSProperties['height'];
 }
 
 export const StyledTable = styled.table<StyledTableProps>`
   position: relative;
-  height: ${({ $height }) => ($height ? $height : 'auto')};
-  width: ${({ $width }) => ($width ? $width : '100%')};
+  height: ${({ $height }) => $height || 'auto'};
+  width: ${({ $width }) => $width || '100%'};
 `;
