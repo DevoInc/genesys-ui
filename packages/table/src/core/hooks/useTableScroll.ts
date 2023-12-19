a93332dc225fea8124186195dd5c98e50b827aba
Carlos Pinedo <carlos.pinedo@devo.com>
2023-12-19 18:12:04 +0100

    fix(Table): QUV-1922 Scroll styles behavior

--- packages/table/src/core/hooks/useTableScroll.ts
+++ packages/table/src/core/hooks/useTableScroll.ts
@@ -9,29 +9,43 @@ export const useTableScroll = (
   const [wrapperHeight, setWrapperHeight] = React.useState<number>(0);
   const [headHeight, setHeadHeight] = React.useState<number>(0);
   const [hasScroll, setHasScroll] = React.useState<boolean>(false);
-  React.useEffect(() => {
-    console.debug(rowVirtualizer.getTotalSize());
-    setDataHeight(rowVirtualizer.getTotalSize());
+
+  const handleResize = () => {
+    /* The wrapper height is actually conditioned by the window height, as it
+          can be lower than the table wrapper */
     setWrapperHeight(
-      wrapperRef.current?.querySelector<HTMLElement>('table')?.offsetHeight,
+      Math.max(
+        wrapperRef.current?.querySelector<HTMLElement>('table')?.offsetHeight,
+        window.innerHeight,
+      ),
     );
+  };
+
+  React.useEffect(() => {
+    setDataHeight(rowVirtualizer.getTotalSize());
     setHeadHeight(
       wrapperRef.current?.querySelector<HTMLElement>('table thead')
         ?.offsetHeight,
     );
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   React.useEffect(() => {
-    console.debug(
-      'dataHeight, wrapperHeight, headHeight',
-      dataHeight,
-      wrapperHeight,
-      headHeight,
-    );
-    console.debug('hasScroll', dataHeight > wrapperHeight - headHeight);
-    setHasScroll(dataHeight > wrapperHeight - headHeight);
-  }, [dataHeight, wrapperHeight, headHeight]);
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  React.useEffect(
+    () => setHasScroll(dataHeight > wrapperHeight - headHeight),
+    [dataHeight, wrapperHeight, headHeight],
+  );
 
   return { hasScroll };
 };
