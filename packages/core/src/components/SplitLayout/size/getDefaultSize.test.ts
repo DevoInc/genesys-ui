737844920e7ffc1d0b35298ae768f8935267c09d
Javier López Úbeda <jlopezcur@gmail.com>
2024-07-26 10:17:50 +0200

    test: fix failing test

--- packages/core/src/components/SplitLayout/size/getDefaultSize.test.ts
+++ packages/core/src/components/SplitLayout/size/getDefaultSize.test.ts
@@ -1,13 +1,14 @@
 import { describe, test, expect } from 'vitest';
 
 import { getDefaultSize } from './getDefaultSize';
+import type { TSizes } from '../declarations';
 
 describe('components', () => {
   describe('SplitLayout', () => {
     describe('size', () => {
       describe('getDefaultSize', () => {
-        const cases: [string, number, number[]][] = [
-          ['4 children', 4, [25, 25, 25, 25]],
+        const cases: [string, number, TSizes][] = [
+          ['4 children', 4, ['25%', '25%', '25%', '25%']],
         ];
         test.each(cases)('%s', (_title, childrenNumber, expected) => {
           expect(getDefaultSize(childrenNumber)).toStrictEqual(expected);
