d81a22f71297832ea8d49c7ac91e610846adf814
santiago.trigo <santiago.trigo@devo.com>
2024-10-08 12:06:48 +0200

    fix: delete z index isAfter row

--- packages/table/src/presets/row/isAfterRow.tsx
+++ packages/table/src/presets/row/isAfterRow.tsx
@@ -5,7 +5,6 @@ export const isAfterRow: TRowPreset = {
   style: ({ theme, evenOddType }) => css`
     background-color: ${theme.cmp.table.row.color.background[evenOddType]
       .afterRow};
-    z-index: ${theme.cmp.table.row.elevation.zIndex.isAfterRow};
   `,
   id: 'isAfterRow',
 };
