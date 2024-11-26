5fadc1f56c7432156ea3775ea5676289a4405389
trigoporres <santiagotrigoporres@gmail.com>
2024-11-26 12:47:46 +0100

    doc: add cell expand storie

---
+++ packages/table/src/wrapper/CellExpandWrapper/AllTypesColumn.tsx
@@ -0,0 +1,119 @@
+import { TColDef } from '../../../src';
+import { ROW_HEIGHT_MD } from '../../../src/constants';
+
+export const AllTypesColumn: TColDef[] = [
+  {
+    id: 'id',
+    preset: 'text',
+    headerName: 'ID',
+    sortable: true,
+  },
+  {
+    id: 'booleanValue',
+    headerName: 'Boolean value',
+    preset: 'boolean',
+    sortable: true,
+  },
+  {
+    id: 'name',
+    headerName: 'Name',
+    preset: 'text',
+    sortable: true,
+  },
+  {
+    id: 'age',
+    headerName: 'Age',
+    preset: 'number',
+    sortable: true,
+  },
+  {
+    id: 'company',
+    headerName: 'Company',
+    preset: 'text',
+    sortable: true,
+  },
+  {
+    id: 'balance',
+    headerName: 'Balance',
+    preset: 'number',
+    sortable: true,
+  },
+  {
+    id: 'status',
+    headerName: 'Status',
+    preset: 'options',
+    context: {
+      options: {
+        done: { colorScheme: 'success' },
+        test: { colorScheme: 'warning' },
+        TODO: { colorScheme: 'data-purple' },
+        inProgress: { colorScheme: 'data-blue' },
+      },
+    },
+    sortable: true,
+  },
+  {
+    id: 'picture',
+    headerName: 'Image (URL)',
+    preset: 'link',
+    sortable: true,
+  },
+  {
+    id: 'timestamp',
+    headerName: 'Date',
+    preset: 'date',
+    sortable: true,
+  },
+  {
+    id: 'tags',
+    headerName: 'Tags',
+    preset: 'options',
+    context: {
+      options: {
+        Coworker: { colorScheme: 'success' },
+        Developer: { colorScheme: 'data-magenta' },
+        Engineer: { colorScheme: 'data-purple' },
+        Components: { colorScheme: 'data-blue' },
+      },
+    },
+    sortable: true,
+  },
+  {
+    id: 'profession',
+    headerName: 'Profession',
+    preset: 'text',
+    sortable: true,
+  },
+  {
+    id: 'email',
+    headerName: 'Email',
+    preset: 'link',
+    sortable: true,
+  },
+  {
+    id: 'quote',
+    headerName: 'Favourite quote',
+    preset: 'longText',
+    rowHeight: ROW_HEIGHT_MD,
+    truncateLine: 1,
+    sortable: true,
+  },
+  {
+    id: 'address',
+    headerName: 'Address',
+    preset: 'text',
+    sortable: true,
+  },
+  {
+    id: 'website',
+    headerName: 'Website',
+    preset: 'link',
+    sortable: true,
+  },
+  {
+    id: 'secondaryWebsite',
+    headerName: 'Secondary Website',
+    preset: 'link',
+    sortable: true,
+  },
+];
---
+++ packages/table/src/wrapper/CellExpandWrapper/CellExpand.stories.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Flex } from '@devoinc/genesys-ui';
+import { BasicTable } from '../../recipes';
+import { useOrderStruct } from '../../hooks';
+import { orderDataByOrderStruct, updateColDefsWithOrderStruct } from '../../helpers';
+import { TColDef } from '../../declarations';
+import { ROW_HEIGHT_MD } from '../../constants';
+import { allTypesData } from '../../../stories/table/data/allTypesData';
+import { AllTypesColumn } from './AllTypesColumn';
+
+
+const meta: Meta<typeof BasicTable> = {
+  title: 'Components/Layout/Table/Cell/Expand',
+  component: BasicTable,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof BasicTable>;
+
+const BasicCmp = ({ data, colDefs }) => {
+  const [newData, setNewData] = React.useState(data);
+  const { orderStruct, onSorting } = useOrderStruct([{ id: 'id', sort: 'desc' }]);
+  React.useEffect(() => {
+    setNewData([...newData].sort(orderDataByOrderStruct(orderStruct)));
+  }, [onSorting]);
+  return (
+    <Flex flexDirection="column" gap="cmp-md" height={'auto'}>
+      <Flex.Item>
+        <BasicTable
+          id={'basicTableStorie'}
+          onSort={(colDef: TColDef) => {
+            onSorting(colDef.id);
+          }}
+          texts={{
+            cell: {
+              editTooltip: 'editTooltip',
+            },
+          }}
+          textsCell={({ rowIndex }) =>
+            rowIndex % 2 === 0 ? 'soy par' : 'soy impar'
+          }
+          data={newData}
+          colDefs={updateColDefsWithOrderStruct(colDefs, orderStruct)}
+          defaultColDef={{
+            isExpandable: true,
+          }}
+          maxHeight="80vh"
+          minWidth={2800}
+          rowHeight={ROW_HEIGHT_MD}
+          highlightRowOnHover={true}
+          showFilters={true}
+          onCellDataChange={({ colDef, value, rowIndex }) => {
+            setNewData(
+              newData.map((data, index) => {
+                if (index === rowIndex) {
+                  data[colDef.id] = value;
+                }
+                return data;
+              }),
+            );
+          }}
+          onSorting={onSorting}
+        />
+      </Flex.Item>
+    </Flex>
+  );
+};
+
+export const CellExpand: Story = {
+  render: () => <BasicCmp data={allTypesData} colDefs={AllTypesColumn} />,
+};
