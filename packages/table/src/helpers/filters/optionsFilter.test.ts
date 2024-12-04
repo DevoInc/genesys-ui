62f9a726be06fb9a66865c1e1abab29cbc95fbce
Javier López Úbeda <jlopezcur@gmail.com>
2024-12-04 11:59:18 +0100

    fix(table): added missing fn options filter for multi options

---
+++ packages/table/src/helpers/filters/optionsFilter.test.ts
@@ -0,0 +1,31 @@
+import { describe, test, expect } from 'vitest';
+
+import { optionsFilter } from './optionsFilter';
+import { TOptionsFilterValue } from '../../filters';
+
+describe('helpers', () => {
+  describe('filters', () => {
+    describe('optionsFilter', () => {
+      const cases: [string, string | string[], TOptionsFilterValue, boolean][] =
+        [
+          ['string', 'abc', { value: [{ label: 'abc', value: 'abc' }] }, true],
+          [
+            'truth array of string',
+            ['abc', 'def'],
+            { value: [{ label: 'abc', value: 'abc' }] },
+            true,
+          ],
+          [
+            'falsy array of string',
+            ['abc', 'def'],
+            { value: [{ label: 'xyz', value: 'xyz' }] },
+            false,
+          ],
+        ];
+
+      test.each(cases)('%s', (_title, data, options, expected) => {
+        expect(optionsFilter(data, options)).toBe(expected);
+      });
+    });
+  });
+});
