6221f15be43622a5514ad8fafed5aa49186df192
Javier López Úbeda <jlopezcur@gmail.com>
2024-12-10 12:42:38 +0100

    fix(core): fix SplitLayout gutter size not subtracted from sizes

--- packages/core/src/components/SplitLayout/size/getSizesWithSeparators.ts
+++ packages/core/src/components/SplitLayout/size/getSizesWithSeparators.ts
@@ -2,4 +2,8 @@ import { interpolateItems } from '../array';
 import type { TSizes } from '../declarations';
 
 export const getSizesWithSeparators = (sizes: TSizes, separatorSize: number) =>
-  interpolateItems(sizes, () => separatorSize) as TSizes;
+  interpolateItems(
+    sizes,
+    () => separatorSize,
+    (x: number) => x - separatorSize / 2,
+  ) as TSizes;
