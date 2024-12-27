6cdf3871b68df9a37a9eca593fdf8cd99d584b0b
Javier López Úbeda <jlopezcur@gmail.com>
2024-12-27 10:42:48 +0100

    chore: removed depcruise because of lack of use

--- depcruise.sh
+++
@@ -1,7 +0,0 @@
-PACKAGE=`echo $1 | rev | cut -d/ -f1 | rev`
-npx depcruise --no-config -T json -f depcruise.json -S 3 -x "node_modules\/(?!@devo)" src/index.ts
-npx depcruise-fmt -T dot depcruise.json | node ../../dot.mjs > ../../.storybook/assets/images/depcruise-simple-$PACKAGE.svg
-rm depcruise.json
-npx depcruise --no-config -T json -f depcruise.json -S "node_modules\/@devo[^/]*\/[^\/]+\/" -x "node_modules\/(?!@devo)" src/index.ts
-npx depcruise-fmt -T dot depcruise.json | node ../../dot.mjs > ../../.storybook/assets/images/depcruise-extended-$PACKAGE.svg
-rm depcruise.json
