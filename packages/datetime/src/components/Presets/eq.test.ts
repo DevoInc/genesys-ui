95010d71ffdbbdb6dbfb744a9e058e373cb843d3
Javier López Úbeda <jlopezcur@gmail.com>
2025-01-03 00:01:57 +0100

    fix(datetime): added missing files for presets equality

---
+++ packages/datetime/src/components/Presets/eq.test.ts
@@ -0,0 +1,46 @@
+import { describe, test, expect } from 'vitest';
+
+import { arePresetValuesEqual } from './eq';
+import type { TDateRange } from '../../declarations';
+
+describe('Presets', () => {
+  describe('eq', () => {
+    describe('arePresetsEqual', () => {
+      const cases: [string, TDateRange, TDateRange, boolean][] = [
+        ['eq num', [1, 2], [1, 2], true],
+        ['eq string', ['1', '2'], ['1', '2'], true],
+        [
+          'eq dates',
+          [new Date(2000, 1, 1), new Date(2000, 1, 2)],
+          [new Date(2000, 1, 1), new Date(2000, 1, 2)],
+          true,
+        ],
+        ['not eq num', [1, 2], [2, 2], false],
+        ['not eq string', ['1', '2'], ['2', '2'], false],
+        [
+          'not eq dates',
+          [new Date(2000, 1, 1), new Date(2000, 1, 2)],
+          [new Date(2001, 1, 1), new Date(2000, 1, 2)],
+          false,
+        ],
+        ['not eq mix num & string', [1, 2], ['1', '2'], false],
+        [
+          'not eq mix num & date',
+          [1, 2],
+          [new Date(2001, 1, 1), new Date(2000, 1, 2)],
+          false,
+        ],
+        [
+          'not eq mix string & date',
+          ['1', '2'],
+          [new Date(2001, 1, 1), new Date(2000, 1, 2)],
+          false,
+        ],
+      ];
+
+      test.each(cases)('%s', (_title, a, b, expected) => {
+        expect(arePresetValuesEqual(a, b)).toBe(expected);
+      });
+    });
+  });
+});
---
+++ packages/datetime/src/components/Presets/eq.ts
@@ -0,0 +1,12 @@
+import type { TDateRange } from '../../declarations';
+
+export const arePresetValuesEqual = (a: TDateRange, b: TDateRange) =>
+  a.every((item, index) =>
+    typeof item === typeof b[index]
+      ? typeof item === 'number' || typeof item === 'string'
+        ? item === b[index]
+        : item instanceof Date && b[index] instanceof Date
+          ? item.getTime() === b[index].getTime()
+          : false
+      : false,
+  );
