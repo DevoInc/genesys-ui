7859ec1e9905d57f6672a63c696f1ea13dd76126
Jose M. Esteban <josem.esteban@devo.com>
2025-03-14 13:27:51 +0100

    fix(table): Multiple options filter

--- packages/table/src/helpers/filters/optionsFilter.test.ts
+++ packages/table/src/helpers/filters/optionsFilter.test.ts
@@ -8,11 +8,56 @@ describe('helpers', () => {
     describe('optionsFilter', () => {
       const cases: [string, string | string[], TOptionsFilterValue, boolean][] =
         [
-          ['string', 'abc', { value: [{ label: 'abc', value: 'abc' }] }, true],
           [
-            'truth array of string',
+            'truth string',
+            'abc',
+            { value: [{ label: 'abc', value: 'abc' }] },
+            true,
+          ],
+          [
+            'falsy string',
+            'abc',
+            { value: [{ label: 'ab', value: 'ab' }] },
+            false,
+          ],
+          [
+            'the array contains at least one element of the filter',
             ['abc', 'def'],
             { value: [{ label: 'abc', value: 'abc' }] },
+            true,
+          ],
+          [
+            'the array contains all of the elements of the filter',
+            ['abc', 'def'],
+            {
+              value: [
+                { label: 'abc', value: 'abc' },
+                { label: 'def', value: 'def' },
+              ],
+            },
+            true,
+          ],
+          [
+            'the array does not contain all of the elements of the filter',
+            ['abc', 'def'],
+            {
+              value: [
+                { label: 'abc', value: 'abc' },
+                { label: 'ghi', value: 'ghi' },
+              ],
+            },
+            false,
+          ],
+          [
+            'the filter contains all of the elements of the array and something more',
+            ['abc', 'def'],
+            {
+              value: [
+                { label: 'abc', value: 'abc' },
+                { label: 'def', value: 'def' },
+                { label: 'ghi', value: 'ghi' },
+              ],
+            },
             false,
           ],
           [
--- packages/table/src/helpers/filters/optionsFilter.ts
+++ packages/table/src/helpers/filters/optionsFilter.ts
@@ -1,10 +1,10 @@
 import type { TOptionsFilterValue } from '../../filters';
 
 const compareArray = (array1, array2) => {
-  if (array1.length !== array2.length) {
+  if (array1.length < array2.length) {
     return false;
   }
-  return array1.every((elemento) => array2.includes(elemento));
+  return array2.every((elemento) => array1.includes(elemento));
 };
 
 export const optionsFilter = (
