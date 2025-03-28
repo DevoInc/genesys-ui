11ca3b13c950c0c7bfb1ce577fa2d013a629c1c7
Jose Andrés Granero <joseandres.granero@devo.com>
2025-03-28 10:14:55 +0100

    fix(Table): Fixed problem with flickering by detecting overflow in the options cell with a useEffect instead of pure css.

--- packages/table/src/renderers/OptionsRenderer/OptionsRenderer.tsx
+++ packages/table/src/renderers/OptionsRenderer/OptionsRenderer.tsx
@@ -5,9 +5,8 @@ import { Flex, Tag, TagGroup, Typography } from '@devoinc/genesys-ui';
 
 import type { TCellRenderer } from '../../declarations';
 import type { TContextOptions, TContextOption } from './declarations';
-import { StyledOptionsRendererWrapper } from './StyledOptionsRendererWrapper';
 import { TableContext } from '../../context';
-import { remToPx } from 'polished';
+import { getPxFromRem } from '@devoinc/genesys-ui';
 
 export const OptionsRenderer: React.FC<TCellRenderer> = ({
   value = [],
@@ -21,19 +20,28 @@ export const OptionsRenderer: React.FC<TCellRenderer> = ({
   const options = context?.options ?? [];
   const flexWrap = context?.flexWrap ?? 'nowrap';
   const theme = useTheme();
-  const evalHorPadding = parseFloat(
-    remToPx(theme.cmp.table.cell.space.padding.hor[density]),
+  const evalHorPadding = getPxFromRem(
+    theme.cmp.table.cell.space.padding.hor[density],
   );
   const contentEvalWidth = width - evalHorPadding;
-  const ellipsisMarkerWidth = 32;
+  const contentRef = React.createRef<HTMLDivElement>();
+  const [contentOverflow, setContentOverflow] = React.useState(false);
+
+  React.useEffect(() => {
+    const contentWidth = contentRef.current.clientWidth;
+    if (contentWidth > contentEvalWidth) {
+      setContentOverflow(true);
+    }
+  }, [contentRef, contentEvalWidth]);
 
   return (
     <Flex overflow="hidden" flex="1">
-      <StyledOptionsRendererWrapper
-        $width={contentEvalWidth}
-        $ellipsisMarkerWidth={ellipsisMarkerWidth}
+      <Flex
+        position="relative"
+        overflowX="auto"
+        style={`&::-webkit-scrollbar {display: none;}`}
       >
-        <TagGroup flexWrap={flexWrap}>
+        <TagGroup flexWrap={flexWrap} ref={contentRef}>
           {realValue.map((tag: string) => {
             const option = Object.entries(options).find(([k]) => k === tag);
             const optionParams = option ? (option[1] as TContextOption) : {};
@@ -47,9 +55,9 @@ export const OptionsRenderer: React.FC<TCellRenderer> = ({
             );
           })}
         </TagGroup>
-      </StyledOptionsRendererWrapper>
-      {!cellDef?.isExpanded && (
-        <Flex width={`${ellipsisMarkerWidth}px`} justifyContent="center">
+      </Flex>
+      {!cellDef?.isExpanded && contentOverflow && (
+        <Flex flex="0 0 auto" justifyContent="center" marginLeft="cmp-xs">
           <Typography.Paragraph size="xl">...</Typography.Paragraph>
         </Flex>
       )}
