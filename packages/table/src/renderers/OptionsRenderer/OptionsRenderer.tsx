f85fbe527668c1c2a36ba3aa1fc9d658800f99b7
Jose Andrés Granero <joseandres.granero@devo.com>
2025-04-02 13:29:30 +0200

    feat(Table): Use of pure css to detect scroll and add ellipsis symbol on Tags column.

--- packages/table/src/renderers/OptionsRenderer/OptionsRenderer.tsx
+++ packages/table/src/renderers/OptionsRenderer/OptionsRenderer.tsx
@@ -5,8 +5,9 @@ import { Flex, Tag, TagGroup, Typography } from '@devoinc/genesys-ui';
 
 import type { TCellRenderer } from '../../declarations';
 import type { TContextOptions, TContextOption } from './declarations';
+import { StyledOptionsRendererWrapper } from './StyledOptionsRendererWrapper';
 import { TableContext } from '../../context';
-import { getPxFromRem } from '@devoinc/genesys-ui';
+import { remToPx } from 'polished';
 
 export const OptionsRenderer: React.FC<TCellRenderer> = ({
   value = [],
@@ -20,28 +21,19 @@ export const OptionsRenderer: React.FC<TCellRenderer> = ({
   const options = context?.options ?? [];
   const flexWrap = context?.flexWrap ?? 'nowrap';
   const theme = useTheme();
-  const evalHorPadding = getPxFromRem(
-    theme.cmp.table.cell.space.padding.hor[density],
+  const evalHorPadding = parseFloat(
+    remToPx(theme.cmp.table.cell.space.padding.hor[density]),
   );
-  const contentEvalWidth = width - evalHorPadding;
-  const contentRef = React.createRef<HTMLDivElement>();
-  const [contentOverflow, setContentOverflow] = React.useState(false);
-
-  React.useEffect(() => {
-    const contentWidth = contentRef.current.clientWidth;
-    if (contentWidth > contentEvalWidth) {
-      setContentOverflow(true);
-    }
-  }, [contentRef, contentEvalWidth]);
+  const contentEvalWidth = width - evalHorPadding * 2;
+  const ellipsisMarkerWidth = 32;
 
   return (
     <Flex overflow="hidden" flex="1">
-      <Flex
-        position="relative"
-        overflowX="auto"
-        style={`&::-webkit-scrollbar {display: none;}`}
+      <StyledOptionsRendererWrapper
+        $width={contentEvalWidth}
+        $ellipsisMarkerWidth={ellipsisMarkerWidth}
       >
-        <TagGroup flexWrap={flexWrap} ref={contentRef}>
+        <TagGroup flexWrap={flexWrap}>
           {realValue.map((tag: string) => {
             const option = Object.entries(options).find(([k]) => k === tag);
             const optionParams = option ? (option[1] as TContextOption) : {};
@@ -55,9 +47,9 @@ export const OptionsRenderer: React.FC<TCellRenderer> = ({
             );
           })}
         </TagGroup>
-      </Flex>
-      {!cellDef?.isExpanded && contentOverflow && (
-        <Flex flex="0 0 auto" justifyContent="center" marginLeft="cmp-xs">
+      </StyledOptionsRendererWrapper>
+      {!cellDef?.isExpanded && (
+        <Flex width={`${ellipsisMarkerWidth}px`} justifyContent="center">
           <Typography.Paragraph size="xl">...</Typography.Paragraph>
         </Flex>
       )}
