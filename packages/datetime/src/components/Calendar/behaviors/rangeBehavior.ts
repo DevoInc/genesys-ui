bf94cc7b40cf6f3c6b9e6eb3c7ff6b856677649f
Javier López Úbeda <jlopezcur@gmail.com>
2025-05-28 18:24:58 +0200

    fix(datetime): fix range behavior on calendar ranges

--- packages/datetime/src/components/Calendar/behaviors/rangeBehavior.ts
+++ packages/datetime/src/components/Calendar/behaviors/rangeBehavior.ts
@@ -1,10 +1,33 @@
-import { compareDesc } from 'date-fns';
+import { compareDesc, set } from 'date-fns';
+import { tz as tzFn } from '@date-fns/tz';
 
-import type { TCalendarDate, TCalendarDateRange } from '../../../declarations';
+import type {
+  TCalendarDate,
+  TCalendarDateRange,
+  IEndPointRangeTime,
+} from '../../../declarations';
 
-export const rangeBehavior = (range: TCalendarDateRange, dt: TCalendarDate) =>
+export const rangeBehavior = ({
+  range,
+  dt,
+  tz,
+  startRange,
+  endRange,
+}: {
+  range: TCalendarDateRange;
+  dt: TCalendarDate;
+  tz: string;
+  startRange: IEndPointRangeTime;
+  endRange: IEndPointRangeTime;
+}) =>
   range.length === 1
     ? compareDesc(dt, range[0]) < 0
-      ? [range[0], dt]
-      : [dt, range[0]]
+      ? [
+          set(range[0], startRange, { in: tzFn(tz) }).valueOf(),
+          set(dt, endRange, { in: tzFn(tz) }).valueOf(),
+        ]
+      : [
+          set(dt, startRange, { in: tzFn(tz) }).valueOf(),
+          set(range[0], endRange, { in: tzFn(tz) }).valueOf(),
+        ]
     : [dt];
--- packages/datetime/src/components/Calendar/hooks/useCalendarRange.ts
+++ packages/datetime/src/components/Calendar/hooks/useCalendarRange.ts
@@ -2,15 +2,34 @@ import * as React from 'react';
 
 import type { IHookCommonReturnParams } from './declarations';
 import { rangeBehavior } from '../behaviors';
-import type { TCalendarDate, TCalendarDateRange } from '../../../declarations';
+import type {
+  IEndPointRangeTime,
+  TCalendarDate,
+  TCalendarDateRange,
+} from '../../../declarations';
 
 export const useCalendarRange = (
   initialRange: TCalendarDateRange = [],
+  tz: string = Intl.DateTimeFormat().resolvedOptions().timeZone,
+  startRange: IEndPointRangeTime = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0,
+  },
+  endRange: IEndPointRangeTime = {
+    hours: 23,
+    minutes: 59,
+    seconds: 59,
+    milliseconds: 999,
+  },
 ): IHookCommonReturnParams => {
   const [range, setRange] = React.useState(initialRange);
 
   const handleNewDate = (newDate: TCalendarDate) => {
-    setRange((prev) => rangeBehavior(prev, newDate));
+    setRange((prev) =>
+      rangeBehavior({ range: prev, dt: newDate, tz, startRange, endRange }),
+    );
   };
 
   return {
