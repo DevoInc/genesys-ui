0bbc907e8e41f4096447eb9551f5075d16ff3caa
Javier López Úbeda <jlopezcur@gmail.com>
2025-06-16 13:19:14 +0200

    test: vitest change of workspaces to projects

--- vitest.config.ts
+++ vitest.config.ts
@@ -35,5 +35,50 @@ export default defineConfig({
     alias: {
       '@test': resolve(__dirname, './test/test-utils.tsx'),
     },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'code',
+          root: 'packages/code',
+          setupFiles: [resolve(__dirname, './vitest.setup.ts')],
+          alias: {
+            'monaco-editor-core': resolve(
+              __dirname,
+              './node_modules/monaco-editor-core/esm/vs/editor/editor.main',
+            ),
+          },
+          deps: { optimizer: { web: { include: ['vitest-canvas-mock'] } } },
+          poolOptions: { threads: { singleThread: true } },
+        },
+      },
+      {
+        extends: true,
+        test: { name: 'color', root: 'packages/color' },
+      },
+      {
+        extends: true,
+        test: { name: 'core', root: 'packages/core' },
+      },
+      {
+        extends: true,
+        test: { name: 'datetime', root: 'packages/datetime' },
+      },
+      {
+        extends: true,
+        test: { name: 'table', root: 'packages/table' },
+      },
+      // {
+      //   extends: true,
+      //   test: { name: 'upload', root: 'packages/upload',
+      //     alias: {
+      //       'react-filepond': resolve(
+      //         __dirname,
+      //         './node_modules/react-filepond/dist/react-filepond.esm',
+      //       ),
+      //     },
+      //   },
+      // },
+    ],
   },
 });
